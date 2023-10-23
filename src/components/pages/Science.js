566e42b6381f86621fea30a7a9c61ab1414af84d
ardaberk <ardaberk.duzgun@gmail.com>
2023-10-23 20:06:18 +0300

    Custom component added according to DRY principle

--- src/components/pages/Science.js
+++ src/components/pages/Science.js
@@ -4,6 +4,7 @@ import { Grid, Paper, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import Pagination from '@mui/material/Pagination';
 import CustomCardMedia from '../common/CustomCardMedia'
+import DisplayedNewsGrid from '../common/DisplayedNewsGrid';
 
 const Science = () => {
   const [news, setNews] = useState([]);
@@ -43,43 +44,19 @@ const Science = () => {
     });
 }, []);
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
+  const handleChangePage = (newPageValue) => {
+    setPage(newPageValue);
   };
 
   const startIndex = (page - 1) * articlesPerPage;
   const endIndex = startIndex + articlesPerPage;
   const displayedNews = news.slice(startIndex, endIndex);
 
-  return (
-    <Grid className="news-container" sx={{paddingLeft:1}}>
-      <Grid container spacing={2}>
-        {displayedNews.map((article, index) => (
-          <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
-            <Paper elevation={3} className="news-article">
-              <h2>{article.title || article.webTitle}</h2>
-              <p>{article.description || article.abstract}</p>
-              <Button component={Link} to={article.url || article.webUrl} target="_blank" rel="noopener noreferrer">
-                  Learn More
-                </Button>
-              <CustomCardMedia article={article}/>
-            </Paper>
-            {(index + 1) % 4 === 0 && (
-              <Box sx={{ borderTop: 1, borderColor: 'divider' }} mt={2} mb={2} display={{ xs: 'none', sm: 'none', md: 'inline-block', lg:'inline-block' }} />
-            )}
-          </Grid>
-        ))}
-      </Grid>
-      <Pagination
-        count={Math.ceil(news.length / articlesPerPage)}
-        page={page}
-        onChange={handleChangePage}
-        color="primary"
-        size="large"
-        style={{ marginTop: '20px', display: 'flex', justifyContent: 'center' }}
-      />
-    </Grid>
-  );
+  return (<DisplayedNewsGrid displayedNews = {displayedNews}
+    sendPageChange = {handleChangePage} 
+    news = {news}
+    articlesPerPage = {articlesPerPage} 
+    page = {page}/>);
 };
 
 export default Science;
